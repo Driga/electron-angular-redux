e4a75898a090c5cfacada4289db177c2e9f35104
vdrigapentalog <vdriga@pentalog.com>
2018-05-16 16:37:54 +0300

    App starter kit with Angular and Electron set up.

---
+++ main.js
@@ -0,0 +1,134 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+// this should be placed at top of main.js to handle setup events quickly
+// if (handleSquirrelEvent()) {
+//   // squirrel event handled and app will exit in 1000ms, so don't do anything else
+//   return
+// }
+
+let win
+
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: '#ffffff',
+        icon: `file://${__dirname}/dist/assets/logo.png`
+    })
+
+    let menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                {label: 'Adjust notifications'},
+                {label: 'Cap'},
+                {type: 'separator'},
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit()
+
+                    }
+                }
+
+            ]
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu)
+
+    win.loadURL(`file://${__dirname}/dist/index.html`)
+
+
+    //// uncomment below to open the DevTools.
+    // win.webContents.openDevTools()
+
+    // Event when the window is closed.
+    win.on('closed', function () {
+        win = null
+    })
+}
+
+// Create window on electron intialization
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+    // On macOS specific close process
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    // macOS specific close process
+    if (win === null) {
+        createWindow()
+    }
+})
+
+//
+// function handleSquirrelEvent() {
+//   if (process.argv.length === 1) {
+//     return false
+//   }
+//
+//   const ChildProcess = require('child_process')
+//   const path = require('path')
+//
+//   const appFolder = path.resolve(process.execPath, '..')
+//   const rootAtomFolder = path.resolve(appFolder, '..')
+//   const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'))
+//   const exeName = path.basename(process.execPath)
+//
+//   const spawn = function (command, args) {
+//     let spawnedProcess, error
+//
+//     try {
+//       spawnedProcess = ChildProcess.spawn(command, args, {detached: true})
+//     } catch (error) {
+//     }
+//
+//     return spawnedProcess
+//   }
+//
+//   const spawnUpdate = function (args) {
+//     return spawn(updateDotExe, args)
+//   }
+//
+//   const squirrelEvent = process.argv[1]
+//   switch (squirrelEvent) {
+//     case '--squirrel-install':
+//     case '--squirrel-updated':
+//       // Optionally do things such as:
+//       // - Add your .exe to the PATH
+//       // - Write to the registry for things like file associations and
+//       //   explorer context menus
+//
+//       // Install desktop and start menu shortcuts
+//       spawnUpdate(['--createShortcut', exeName])
+//
+//       setTimeout(app.quit, 1000)
+//       return true
+//
+//     case '--squirrel-uninstall':
+//       // Undo anything you did in the --squirrel-install and
+//       // --squirrel-updated handlers
+//
+//       // Remove desktop and start menu shortcuts
+//       spawnUpdate(['--removeShortcut', exeName])
+//
+//       setTimeout(app.quit, 1000)
+//       return true
+//
+//     case '--squirrel-obsolete':
+//       // This is called on the outgoing version of your app before
+//       // we update to the new version - it's the opposite of
+//       // --squirrel-updated
+//
+//       app.quit()
+//       return true
+//   }
+// }
